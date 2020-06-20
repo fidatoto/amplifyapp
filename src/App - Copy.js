efe7978f7e8ec11f54d3e707db47a0d6623b8955
@fidatoto <@fidatoto>
2020-06-20 16:45:21 -0500

    changes for v2 de la aplicacion Fidel Torres

--- src/App - Copy.js
+++ src/App - Copy.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>HOLA NUEVA APP - FIDEL TORRES</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
