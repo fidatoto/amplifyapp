ef0d29d693683d77eb5513ffec4f9da3001b1ac8
fidatoto <67205434+fidatoto@users.noreply.github.com>
2020-06-20 18:58:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
